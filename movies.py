0e3284f0822c03c156880eee167e34866b4a5338
alohsoen <55198731+alohsoen@users.noreply.github.com>
2022-10-06 14:20:48 +0300

    Add files via upload

---
+++ movies.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import schedule
+import time as tm
+
+url = 'https://m.imdb.com/list/ls073877946/'
+
+response = requests.get(url)
+
+list = []
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+movie_data = soup.findAll('div', attrs= {'class': 'col-xs-12 col-md-6 lister-item mode-simple'})
+for store in movie_data:
+       data = {};
+       img_list = store.find("img")
+       movieTitle = img_list.get("alt")
+       poster = img_list.get("data-src-x2")
+       id = img_list.get("data-tconst")
+       year = store.find('span', class_ = "nowrap").text
+       time = store.p.find('span', class_ = "runtime").text
+       ratings = store.p.find('span', class_ ="genre").text.replace('\n', "").replace(' ', "")
+       rating = store.find('span', class_ ="imdb-rating").text
+       data = {'poster':poster, 'name':movieTitle, 'id':id, 'year':year, 'time':time, 'ratings':ratings, 'rating':rating}
+       list.append(data)
+       json_dump = json.dumps(list)
+       
+def movie():
+      datamv = json_dump
+      return datamv
+def mv_id():
+      mid= id
+      return id
