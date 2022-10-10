71b37a1a98dd39e20d44525c7b63236ea166b57f
alohsoen <55198731+alohsoen@users.noreply.github.com>
2022-10-10 15:24:58 +0300

    Update api.py

--- api.py
+++ api.py
@@ -2,17 +2,19 @@ from flask import *
 import requests
 from bs4 import BeautifulSoup
 import schedule as s
+import threaded
 import time as t
-import json
 import _thread as thread
+from trailer import trailer
+import json
+from info import data_info
 from movies import movie, mv_id
-app = Flask("app")
+app = Flask(__name__)
 def rescrap():
         from movies import movie
-        print("done")
         return movie()
 def ref():
-        s.every(12).hours.do(rescrap)
+        s.every().day.at("12:00").do(rescrap)
         while True:
                      s.run_pending()
                      t.sleep(1)
@@ -20,15 +22,19 @@ thread.start_new_thread(ref, ())
 @app.route("/movie", methods=['POST', 'GET'])
 def movies():
         return movie()
-@app.route("/info/", methods=['POST', 'GET'])
+@app.route("/info", methods=['POST', 'GET'])
 def info():
-      id = str(request.args.get('id'))
-      data = {'id' : id}
-      return data
-@app.route("/search/")
+      uid = str(request.args.get('id'))
+      return data_info(uid)
+@app.route("/search/", methods=['POST', 'GET'])
 def search():
       name = str(request.args.get('name'))
       data = {'name' : name}
       return data
+@app.route("/trailer/", methods=['POST', 'GET'])
+def tr():
+	video_id = str(request.args.get('video_id'))
+	return trailer(video_id)
+
 if __name__ == '__main__' :
         app.run()
