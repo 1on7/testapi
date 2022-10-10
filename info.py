d1b505d883034c352b6bee83b5fbc5f851139ec3
alohsoen <55198731+alohsoen@users.noreply.github.com>
2022-10-10 16:25:53 +0300

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,6 @@ def data_info(uid):
         data = {};
         title = soup.find('h1', class_ ='sc-b73cd867-0 eKrKux').text
         year = soup.find('span', class_ ='sc-8c396aa2-2 itZqyK').text
-        age = soup.find_all('span', class_ = 'sc-8c396aa2-2 itZqyK')[1].text
         run_time = soup.find_all(class_ = 'ipc-inline-list__item')[5].text
         description = soup.find(class_ = 'sc-16ede01-0 fMPjMP').text
         json_list = soup.find_all('script')[1].text
@@ -25,6 +24,10 @@ def data_info(uid):
         reviews = soup.find_all('span', 'score')[0].text
         oscars = soup.find_all('a', 'ipc-metadata-list-item__label ipc-metadata-list-item__label--link')[4].text
         poster = soup.find_all('meta')[9]['content']
+        try :
+           age = soup.find_all('span', class_ = 'sc-8c396aa2-2 itZqyK')[1].text
+        except:
+        	age = ""
         data = {'title':title, 'poster':poster, 'year':year, 'age':age,'time':run_time, 'genre':genre, 'description':description, 'meta_score':meta_score, 'critic_reviews':critic_reviews, 'reviews':reviews, 'oscars':oscars}
         list.append(data)
         json_dump = json.dumps(list)
